649f5e192b822b4a2bd701d36d062a4541830c74
Vita Kotenko <mialkovska.viktoria@gmail.com>
2023-10-05 14:43:22 +0300

    added HW4

---
+++ HW4/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    public class Person
+    {
+        private string name;
+
+        private DateTime birthYear;
+
+        public string Name
+        { get {  return name; } }
+
+        public DateTime BirthYear 
+        { get { return birthYear; } }
+
+        public Person() { 
+            name = "Unknown"; 
+            birthYear = new DateTime(2005,1,1); }
+
+        public Person(string name, DateTime birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        public int Age()
+        { 
+            int age = DateTime.Now.Year - birthYear.Year; 
+            return age;
+        }
+
+        public static Person Input(int i)
+        {
+            Console.WriteLine($"Please enter name {i + 1}th person");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Please enter year of birth {i + 1}th person");
+            DateTime birthYear = new DateTime(Convert.ToInt16(Console.ReadLine()),1,1);
+            Person person = new Person(name, birthYear);
+            return person;
+        }
+
+        public string Output()
+        {
+            return $"{name} was borned in {birthYear.Year}.";
+        }
+
+        public void ChangeName(string newName)
+        {
+            name = newName;
+        }
+
+        public override string ToString() {
+            return $"{name} was borned in {birthYear.Year}";
+        }
+
+        public static bool operator == (Person a, Person b)
+        {
+            return (a.name == b.name);
+        }
+        public static bool operator != (Person a, Person b)
+        {
+            return !(a == b);
+            
+        }
+    }
+
+}
---
+++ HW4/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace HW4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person[] people = new Person[6];
+            people[0] = new Person();
+            people[1] = new Person("Alex", new DateTime(2002,1,1));
+            for (int i = 2; i < people.Length; i++)
+            {
+                
+                people[i] = Person.Input(i);
+            }
+
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.Write(people[i].Output());
+                Console.WriteLine($"She/he is {people[i].Age()} years old.\n");
+            }
+
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (people[i].Age() < 16 )
+                {
+                    people[i].ChangeName("Very Young");
+                }
+                Console.WriteLine(people[i].Output());
+            }
+
+            for (int i = 0;i < people.Length; i++)
+            {
+                for (int j = i+1; j < people.Length; j++) 
+                {
+                    if (people[i] == people[j])
+                    {
+                        Console.WriteLine($"{people[i].ToString()} and {people[j].ToString()} have the same name.\n");
+                    }
+                    
+                }
+            }
+            
+           
+            
+
+        }
+    }
+}
